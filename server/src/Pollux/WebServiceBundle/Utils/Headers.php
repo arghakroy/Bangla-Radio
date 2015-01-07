e91e225759a18769a36aebbddc460168f76ffc53
Kowser <kowsercse@gmail.com>
2015-01-07 00:57:02 -0600

    #5: utility class for Headers

---
+++ server/src/Pollux/WebServiceBundle/Utils/Headers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pollux\WebServiceBundle\Utils;
+
+
+class Headers {
+
+  const CONTENT_TYPE = "Content-Type";
+  const LOCATION = 'Location';
+  const ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+  const WWW_AUTHENTICATE = "WWW-Authenticate";
+
+}
