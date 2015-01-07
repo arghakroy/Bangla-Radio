f5663fda9963f222ace881508925c795d61e6bd1
Kowser <kowsercse@gmail.com>
2015-01-07 00:57:10 -0600

    #5: utility class for MimeTypes

---
+++ server/src/Pollux/WebServiceBundle/Utils/MimeType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pollux\WebServiceBundle\Utils;
+
+
+class MimeType {
+
+  const APPLICATION_JSON = "application/json";
+  const APPLICATION_XML = "application/xml";
+  const APPLICATION_OPEN_SEARCH_DESCRIPTION_JSON = "application/vnd.opensearchdescription+json";
+
+}
