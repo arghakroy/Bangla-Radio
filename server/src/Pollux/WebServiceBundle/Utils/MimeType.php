7b1460df7b14091a5072236af044ddb80b3ccdeb
Kowser <kowsercse@gmail.com>
2015-01-09 10:57:30 -0600

    #5: added text/uri-list mimetype

--- server/src/Pollux/WebServiceBundle/Utils/MimeType.php
+++ server/src/Pollux/WebServiceBundle/Utils/MimeType.php
@@ -8,5 +8,6 @@ class MimeType {
   const APPLICATION_JSON = "application/json";
   const APPLICATION_XML = "application/xml";
   const APPLICATION_OPEN_SEARCH_DESCRIPTION_JSON = "application/vnd.opensearchdescription+json";
+  const TEXT_URI_LIST = 'text/uri-list';
 
 }
