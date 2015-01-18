032811cab1f9ed8d66d8bef5229fd83fef9860fd
Samiron Paul <samiron@polluxlab.com>
2015-01-19 00:21:58 +0600

    getSongs() added in Artist

--- src/main/java/com/polluxlab/banglamusic/model/Artist.java
+++ src/main/java/com/polluxlab/banglamusic/model/Artist.java
@@ -1,7 +1,33 @@
 package com.polluxlab.banglamusic.model;
 
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by samiron on 1/18/2015.
  */
 public class Artist extends ModelBase  {
+    private Links links;
+    private String name;
+    private String summary;
+    private String yearformed;
+    private String placeformed;
+
+    /**
+     *
+     */
+    public List<Song> getSongs(){
+        String response = get(this.links.getSongs());
+        if(response.isEmpty())
+            return new ArrayList<Song>();
+        else {
+            Type songCollectionType = new TypeToken<List<Song>>(){}.getType();
+            return gson.fromJson(response, songCollectionType);
+        }
+    }
+
+
 }
