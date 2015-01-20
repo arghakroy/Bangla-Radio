4aa6a68ced9027191ac817913e4641244bf0398a
Kowser <kowsercse@gmail.com>
2015-01-20 15:50:56 -0600

    #4: use TelenorClient to retrieve token

--- server/src/Pollux/SecurityBundle/Controller/TelenorAuthenticationController.php
+++ server/src/Pollux/SecurityBundle/Controller/TelenorAuthenticationController.php
@@ -6,7 +6,6 @@ namespace Pollux\SecurityBundle\Controller;
 use Pollux\SecurityBundle\Security\Provider\TelenorAuthenticationProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
@@ -37,55 +36,12 @@ class TelenorAuthenticationController extends Controller {
 
     $code = $request->query->get('code');
 
-    $logger->debug("===========================> code $code");
-
-    $token = $this->getToken($code);
+    $token = $this->get('service.telenor.client')->getToken($code);
+    $logger->debug("Code => $code");
+    $logger->debug("Token => $token");
     $this->get('session')->set(TelenorAuthenticationProvider::AUTHORIZATION_CODE_KEY, $token);
 
     return $this->redirectToRoute('webservice.endpoint');
-  }
-
-  public function testAction(Request $request) {
-    $code = $this->get('session')->get(TelenorAuthenticationProvider::AUTHORIZATION_CODE_KEY);
-    var_dump($code);
-    var_dump($request);
-    var_dump($this->getUser());
-
-    return new Response();
-  }
-
-  public function getTokenRedirectAction(Request $request) {
-    $code = $this->get('session')->get(TelenorAuthenticationProvider::AUTHORIZATION_CODE_KEY);
-    var_dump($code);
-    var_dump($request);
-    return new Response();
-  }
-
-  private function getToken($authorizationCode) {
-    $parameters = array(
-        "grant_type" => "authorization_code",
-        "client_id" => $this->container->getParameter('telenor.client.id'),
-        "redirect_uri" => $this->generateUrl('telenor.authentication.getTokenRedirect', null, UrlGeneratorInterface::ABSOLUTE_URL),
-        'code' => $authorizationCode,
-    );
-    $basicAuthentication = $this->container->getParameter('telenor.client.id') . ":" . $this->container->getParameter('telenor.client.secret');
-
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $this->container->getParameter('telenor.url.token'),
-        CURLOPT_POSTFIELDS => $this->prepareQueryUrl($parameters),
-        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-        CURLOPT_USERPWD => $basicAuthentication,
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_CONNECTTIMEOUT => 3,
-        CURLOPT_TIMEOUT => 20
-    ));
-
-    $output = curl_exec($curl);
-    curl_close($curl);
-
-    return $output;
   }
 
   /**
@@ -94,28 +50,19 @@ class TelenorAuthenticationController extends Controller {
    */
   protected function prepareAuthorizeUrl($state) {
     $callbackUrl = $this->generateUrl('telenor.authentication.callback', array(), UrlGeneratorInterface::ABSOLUTE_URL);
-    $callbackUrl = str_replace("localhost", "162.248.162.2", $callbackUrl);
-    $callbackUrl = str_replace("http", "https", $callbackUrl);
     $queryParameters = array(
         'response_type' => 'code',
-        'scope' => 'openid profile email phone',
-        'redirect_uri' => $callbackUrl,
-        'state' => $state,
         'client_id' => $this->container->getParameter('telenor.client.id'),
+        'scope' => 'payment.transactions.read payment.transactions.write payment.agreements.read payment.agreements.write id.user.right.read openid profile',
+        'state' => $state,
+        'redirect_uri' => $callbackUrl,
     );
     $queryParameters = http_build_query($queryParameters);
-    $baseUrl = $this->container->getParameter('telenor.url.authorize');
-    $authorizeUrl = $baseUrl . "?" . $queryParameters;
+
+    $authorizeUrl = $this->get('service.telenor.client')->getAuthorizeUrl() . "?" . $queryParameters;
+    $this->get('logger')->debug("Authorizing with URL: " . $authorizeUrl);
+
     return $authorizeUrl;
-  }
-
-  private function prepareQueryUrl(array $parameters) {
-    $query = '';
-    foreach ($parameters as $key => $value) {
-      $query .= $key . "=" . $value . "&";
-    }
-
-    return $query;
   }
 
 }
