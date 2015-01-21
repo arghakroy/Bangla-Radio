57eb74937faee820e902144ac6adbecbfd1eccc8
Kowser <kowsercse@gmail.com>
2015-01-21 01:22:37 -0600

    #4: add role as constant

---
+++ server/db/values.sql
@@ -0,0 +1,3 @@
+INSERT INTO `role` (`id`, `name`, `detail_name`) VALUES (1,'ROLE_USER', 'ROLE_USER');
+INSERT INTO `role` (`id`, `name`, `detail_name`) VALUES (2,'ROLE_ADMIN', 'ROLE_ADMIN');
+INSERT INTO `role` (`id`, `name`, `detail_name`) VALUES (3,'ROLE_SUPER_ADMIN', 'ROLE_SUPER_ADMIN');
