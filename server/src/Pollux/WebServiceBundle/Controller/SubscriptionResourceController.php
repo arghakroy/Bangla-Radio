ab259f9fd4fb5931a62db8886f40e2e1f9fcae68
Kowser <kowsercse@gmail.com>
2015-01-21 10:38:48 -0600

    #10: the Subscription controller

---
+++ server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Pollux\WebServiceBundle\Controller;
+
+
+use Pollux\WebServiceBundle\Utils\Headers;
+use Pollux\WebServiceBundle\Utils\MimeType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SubscriptionResourceController extends Controller {
+
+  public function getCollectionAction() {
+    $entities = $this->getDoctrine()->getManager()->getRepository('DomainBundle:Subscription')->findAll();
+
+    $response = $this->render('WebServiceBundle:SubscriptionResource:collection.json.twig', array(
+        'entities' => $entities,
+    ));
+    $response->headers->set(Headers::CONTENT_TYPE, MimeType::APPLICATION_JSON);
+
+    return $response;
+  }
+
+  public function getAction($subscriptionId) {
+    $entity = $this->getDoctrine()->getManager()->getRepository('DomainBundle:Subscription')->find($subscriptionId);
+    if (!$entity) {
+      $this->get('logger')->debug("Subscription not found with id: " . $subscriptionId);
+      throw $this->createNotFoundException();
+    }
+
+    $response = $this->render('WebServiceBundle:SubscriptionResource:entity.json.twig', array('entity' => $entity));
+    $response->headers->set(Headers::CONTENT_TYPE, MimeType::APPLICATION_JSON);
+    return $response;
+  }
+
+}
