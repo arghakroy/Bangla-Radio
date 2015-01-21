8df82d3fa315a94498c4679cf3aac5f2921f8f0f
Kowser <kowsercse@gmail.com>
2015-01-20 23:03:52 -0600

    #4: the user table in the sql

--- server/db/tables.sql
+++ server/db/tables.sql
@@ -1,15 +1,40 @@
 -- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+CREATE TABLE `user` (
+  `id`                INT(11)          NOT NULL AUTO_INCREMENT,
+  `username`          VARCHAR(50)
+                      COLLATE utf8_bin NOT NULL,
+  `access_token`      VARCHAR(64)
+                      COLLATE utf8_bin          DEFAULT NULL,
+  `expire_time`       DATETIME                  DEFAULT NULL,
+  `access_token_data` TEXT
+                      COLLATE utf8_bin,
+  `user_info_data`    TEXT
+                      COLLATE utf8_bin NOT NULL,
+  `shared_secret`     VARCHAR(512)
+                      COLLATE utf8_bin          DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_username` (`username`)
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8
+  COLLATE =utf8_bin;
+
+
+-- -----------------------------------------------------
 -- Table `role`
 -- -----------------------------------------------------
 CREATE TABLE `role` (
   `id`          INT(11)            NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45)
+  `name`        VARCHAR(45)
                 CHARACTER SET 'utf8'
                 COLLATE 'utf8_bin' NOT NULL,
-  `detail_name`        VARCHAR(45)
+  `detail_name` VARCHAR(45)
                 CHARACTER SET 'utf8'
                 COLLATE 'utf8_bin' NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`)
+)
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8
   COLLATE = utf8_bin;
@@ -21,7 +46,8 @@ CREATE TABLE `role` (
 CREATE TABLE `user_role` (
   `user` INT(11) NOT NULL,
   `role` INT(11) NOT NULL,
-  UNIQUE KEY `unique_user_role` (`user`, `role`))
+  UNIQUE KEY `unique_user_role` (`user`, `role`)
+)
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8
   COLLATE = utf8_bin;
