baa81afeca54c783855eb5089fb7a52273ec7646
Maz <mazharul2007@gmail.com>
2015-01-22 14:53:56 +0800

    Adding rana/s changes

--- server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
+++ server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
@@ -31,6 +31,8 @@ class SubscriptionResourceController extends Controller {
     //get the `secret` header with the request
     //HTTP_X_SECRET
     $secret = $request->headers->get('x-secret');
+    $user = $this->getDoctrine()->getManager()->getRepository('DomainBundle:User')->getUserFromSecret($sharedSecret);
+    echo $user->getAccessToken();
     
 
     //get the user info from server based on the secret provided by the client
