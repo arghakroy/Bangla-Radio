825069875c2a42976cce7682de7dd0c758a64556
codeftech <joomlacity@gmail.com>
2015-01-22 17:00:07 +0600

    #11: table name product changed to products

--- server/src/Pollux/DomainBundle/Repository/ProductRepository.php
+++ server/src/Pollux/DomainBundle/Repository/ProductRepository.php
@@ -8,6 +8,19 @@ use Doctrine\ORM\Query;
 
 class ProductRepository extends EntityRepository {
 
-  
+  public function getProduct($date) {
+    $query = $this->createQueryBuilder('p')
+        ->select('p')
+        ->where('p.startDate < :currentDate')
+        ->andWhere('p.endDate > :currentDate')
+        ->setParameter('currentDate', $date)
+        ->getQuery();
+
+    $product = NULL;
+    if ($query->getResult() != NULL) {
+      $product = $query->getSingleResult();
+    }
+    return $product;
+  }
 
 }
