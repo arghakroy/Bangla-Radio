1392f1f4eaa1c51c38b6a0dd4b24cc08165e9ef1
Maz <mazharul2007@gmail.com>
2015-01-22 14:31:35 +0800

    Stabing comments for now

--- server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
+++ server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
@@ -6,18 +6,31 @@ namespace Pollux\WebServiceBundle\Controller;
 use Pollux\WebServiceBundle\Utils\Headers;
 use Pollux\WebServiceBundle\Utils\MimeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
 class SubscriptionResourceController extends Controller {
 
   public function getCollectionAction() {
-    $entities = $this->getDoctrine()->getManager()->getRepository('DomainBundle:Subscription')->findAll();
+    /*$entities = $this->getDoctrine()->getManager()->getRepository('DomainBundle:Subscription')->findAll();
 
     $response = $this->render('WebServiceBundle:SubscriptionResource:collection.json.twig', array(
         'entities' => $entities,
     ));
     $response->headers->set(Headers::CONTENT_TYPE, MimeType::APPLICATION_JSON);
 
-    return $response;
+    return $response;*/
+
+    //get the `secret` header with the request
+    
+
+    //get the user info from server based on the secret provided by the client
+    
+
+    //if no `secret` send the client a 412 precondition failed error
   }
 
   public function getAction($subscriptionId) {
