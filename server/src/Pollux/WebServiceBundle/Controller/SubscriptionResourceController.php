469202a631076dd44a045eb8d48ff04d10f3eb6c
Maz <mazharul2007@gmail.com>
2015-01-22 14:37:29 +0800

    Stabing cmment

--- server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
+++ server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
@@ -6,22 +6,28 @@ namespace Pollux\WebServiceBundle\Controller;
 use Pollux\WebServiceBundle\Utils\Headers;
 use Pollux\WebServiceBundle\Utils\MimeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
 class SubscriptionResourceController extends Controller {
 
   public function getCollectionAction() {
-    $entities = $this->getDoctrine()->getManager()->getRepository('DomainBundle:Subscription')->findAll();
 
-    $response = $this->render('WebServiceBundle:SubscriptionResource:collection.json.twig', array(
-        'entities' => $entities,
-    ));
-    $response->headers->set(Headers::CONTENT_TYPE, MimeType::APPLICATION_JSON);
 
-    return $response;
+    //get the `secret` header with the request
+    
+
+    //get the user info from server based on the secret provided by the client
+    
+
+    //if no `secret` send the client a 412 precondition failed error
   }
 
   public function getAction($subscriptionId) {
-    $entity = $this->getDoctrine()->getManager()->getRepository('DomainBundle:Subscription')->find($subscriptionId);
+    /*$entity = $this->getDoctrine()->getManager()->getRepository('DomainBundle:Subscription')->find($subscriptionId);
     if (!$entity) {
       $this->get('logger')->debug("Subscription not found with id: " . $subscriptionId);
       throw $this->createNotFoundException();
@@ -29,7 +35,15 @@ class SubscriptionResourceController extends Controller {
 
     $response = $this->render('WebServiceBundle:SubscriptionResource:entity.json.twig', array('entity' => $entity));
     $response->headers->set(Headers::CONTENT_TYPE, MimeType::APPLICATION_JSON);
-    return $response;
+    return $response;*/
+
+    //get the `secret` header with the request
+    
+
+    //get the user info from server based on the secret provided by the client
+    
+
+    //if no `secret` send the client a 412 precondition failed error
   }
 
 }
