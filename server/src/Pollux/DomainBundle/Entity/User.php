f6db0242d64b7ab8b15fb583270ea1528bd0c03b
Kowser <kowsercse@gmail.com>
2015-01-25 11:50:10 -0600

    #12: expose username as password

--- server/src/Pollux/DomainBundle/Entity/User.php
+++ server/src/Pollux/DomainBundle/Entity/User.php
@@ -255,7 +255,7 @@ class User implements UserInterface, \Serializable {
    * @inheritdoc
    */
   public function getPassword() {
-    return $this->sharedSecret;
+    return $this->username;
   }
 
   /**
