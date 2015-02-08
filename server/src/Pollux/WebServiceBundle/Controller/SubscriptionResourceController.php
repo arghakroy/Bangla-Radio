d1911df1657547c9b357c101d54897cfe80c723b
Kowser <kowsercse@gmail.com>
2015-02-08 12:54:17 -0600

    #11: refactor retrieval of userRight

--- server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
+++ server/src/Pollux/WebServiceBundle/Controller/SubscriptionResourceController.php
@@ -33,24 +33,29 @@ class SubscriptionResourceController extends Controller {
       return new Response('No subscription available.', Response::HTTP_FORBIDDEN);
     }
 
-    $userRight = $userRights->right[0];
-
-    list($startTime, $endTime) = explode("/", $userRight->timeInterval);
-    $startTime = date_create($startTime);
-    $endTime = date_create($endTime);
-    $now = new \DateTime();
-    if($now >= $startTime && $now <= $endTime) {
-      $entity['sku'] = $userRight->sku;
-      $entity['start_date'] = $startTime->format('Y-m-d');
-      $entity['end_date'] = $endTime->format('Y-m-d');
-      $entity['status'] = $userRight->state;
-
-      $response = $this->render('WebServiceBundle:SubscriptionResource:entity.json.twig', array('entity' => $entity));
+    $entry = $this->getUserRightEntry($userRights->right[0]);
+    if($entry) {
+      $response = $this->render('WebServiceBundle:SubscriptionResource:entity.json.twig', array('entity' => $entry));
       $response->headers->set(Headers::CONTENT_TYPE, MimeType::APPLICATION_JSON);
       return $response;
     }
 
     throw $this->createNotFoundException("No active subscription available.");
+  }
+
+  private function getUserRightEntry($userRight) {
+    list($startTime, $endTime) = explode("/", $userRight->timeInterval);
+    $startTime = date_create($startTime);
+    $endTime = date_create($endTime);
+    $now = new \DateTime();
+    if ($now >= $startTime && $now <= $endTime) {
+      $entry['sku'] = $userRight->sku;
+      $entry['start_date'] = $startTime->format('Y-m-d');
+      $entry['end_date'] = $endTime->format('Y-m-d');
+      $entry['status'] = $userRight->state;
+      return $entry;
+    }
+    return null;
   }
 
 }
