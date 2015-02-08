020e7956d8d2c2302dbbdf2d93c06102d38466cd
Kowser <kowsercse@gmail.com>
2015-02-08 00:06:26 -0600

    #11: fixed column name

--- server/src/Pollux/DomainBundle/Repository/ProductRepository.php
+++ server/src/Pollux/DomainBundle/Repository/ProductRepository.php
@@ -3,7 +3,6 @@
 namespace Pollux\DomainBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\Query;
 
 class ProductRepository extends EntityRepository {
@@ -11,7 +10,7 @@ class ProductRepository extends EntityRepository {
   public function getCurrentProduct() {
     $query = $this->createQueryBuilder('p')
         ->select('p')
-        ->where(':currentDate BETWEEN p.start_date AND p.end_date')
+        ->where(':currentDate BETWEEN p.startDate AND p.endDate')
         ->setParameter('currentDate', new \DateTime())
         ->getQuery();
 
