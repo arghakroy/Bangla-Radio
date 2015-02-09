5b95048c65ae8f0243bdcb2355ff7643e01d8291
Kowser <kowsercse@gmail.com>
2015-02-08 21:25:49 -0600

    #18: improve documentation adding hint

--- server/src/Pollux/DomainBundle/Repository/ProductRepository.php
+++ server/src/Pollux/DomainBundle/Repository/ProductRepository.php
@@ -4,9 +4,14 @@ namespace Pollux\DomainBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query;
+use Pollux\DomainBundle\Entity\Product;
 
 class ProductRepository extends EntityRepository {
 
+  /**
+   * @return Product|null
+   * @throws \Doctrine\ORM\NonUniqueResultException
+   */
   public function getCurrentProduct() {
     $query = $this->createQueryBuilder('p')
         ->select('p')
