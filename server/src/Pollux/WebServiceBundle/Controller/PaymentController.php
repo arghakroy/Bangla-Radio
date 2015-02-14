ef16cb7b1fb6632609c1799a75bb05bf753bad1d
Kowser <kowsercse@gmail.com>
2015-02-13 21:14:24 -0600

    #26: use payment ID to retrieve initiate payment information for payment's  - success  - cancelled

--- server/src/Pollux/WebServiceBundle/Controller/PaymentController.php
+++ server/src/Pollux/WebServiceBundle/Controller/PaymentController.php
@@ -3,14 +3,7 @@
 namespace Pollux\WebServiceBundle\Controller;
 
 use Pollux\DomainBundle\Entity\Payment;
-use Pollux\DomainBundle\Entity\User;
-use Pollux\DomainBundle\Repository\ProductRepository;
-use Pollux\WebServiceBundle\Utils\Headers;
-use Pollux\WebServiceBundle\Utils\MimeType;
-use Psr\Log\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class PaymentController extends Controller {
 
@@ -38,20 +31,15 @@ class PaymentController extends Controller {
     return $this->redirect($locationURL);
   }
 
-  public function successAction(Request $request, $paymentId) {
-    /**
-     * @var User $user
-     */
+  public function successAction($paymentId) {
     $em = $this->getDoctrine()->getManager();
-    $telenorClient = $this->get('service.telenor.client');
-    $userId = $request->query->get('user');
-    $user = $this->getDoctrine()->getManager()->getRepository('DomainBundle:User')->findUserByUsername($userId);
-    if(!$user) {
-      $this->get('logger')->debug("No user found with id: $userId");
-      $this->createNotFoundException("No user found with id: $userId");
+    $payment = $em->getRepository('DomainBundle:Payment')->find($paymentId);
+    if(!$payment) {
+      $this->get('logger')->debug('Payment not found with id: ' . $paymentId);
+      throw $this->createNotFoundException('Payment not found with id: ' . $paymentId);
     }
-
-    $userRights = $telenorClient->getUserRights($user);
+    $user = $payment->getUser();
+    $userRights = $this->get('service.telenor.client')->getUserRights($user);
     $this->get('logger')->debug(json_encode($userRights));
 
     $user->setUserRightsData(json_encode($userRights));
@@ -64,6 +52,13 @@ class PaymentController extends Controller {
   }
 
   public function cancelAction($paymentId) {
+    $em = $this->getDoctrine()->getManager();
+    $payment = $em->getRepository('DomainBundle:Payment')->find($paymentId);
+    if(!$payment) {
+      $this->get('logger')->debug('Payment not found with id: ' . $paymentId);
+      throw $this->createNotFoundException('Payment not found with id: ' . $paymentId);
+    }
+
     $url = "polluxmusic://purchase?status=cancelled";
     return $this->redirect($url);
   }
