7a1ad1658634d82b212b4869197ea22b31189165
Kowser <kowsercse@gmail.com>
2015-03-15 05:11:43 -0500

    #38: subscription must be loaded from server

--- musicapp-android/app/src/main/java/com/polluxlab/banglamusic/util/InternalStorageSimple.java
+++ musicapp-android/app/src/main/java/com/polluxlab/banglamusic/util/InternalStorageSimple.java
@@ -1,20 +1,8 @@
 package com.polluxlab.banglamusic.util;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.util.Log;
+import android.content.*;
+import android.util.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.URLEncoder;
-
-/**
- * Created by samiron on 1/25/2015.
- */
 public class InternalStorageSimple {
     private final static String INTERNAL_STG = "INTERNAL_STORAGE";
     private InternalStorageSimple() {}
@@ -37,8 +25,6 @@ public class InternalStorageSimple {
 
         Context context = GlobalContext.get();
         SharedPreferences sh=context.getSharedPreferences(INTERNAL_STG, Context.MODE_PRIVATE);
-        String s = sh.getString(key, "");
-        Log.d(Util.class.getName(), "fetched: " + s);
-        return s;
+        return sh.getString(key, "");
     }
 }
